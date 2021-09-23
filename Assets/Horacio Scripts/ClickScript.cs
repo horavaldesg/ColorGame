400c4feab85f9724b37af4f031942cf96badbe31
Horacio Valdes <horavaldesg@gmail.com>
2021-09-23 11:21:52 -0400

    Footstep Rate

--- Assets/Horacio Scripts/ClickScript.cs
+++ Assets/Horacio Scripts/ClickScript.cs
@@ -4,29 +4,51 @@ using UnityEngine;
 
 public class ClickScript : MonoBehaviour {
     public GameObject ojTransform;
+    public GameObject LeftFootTrnf;
+    public GameObject RightFootTrnf;
+
     public GameObject footStepTexture;
     float footsteps;
     [SerializeField] float footstepRate;
-    float t;
+    float leftT;
+    float rightT;
+    [SerializeField]float altSteps;
+    float tAltSteps;
+
     void Update()
     {
-        //if (Input.GetMouseButtonDown(0))
-        //{
+       
         RaycastHit hit;
         Debug.DrawRay(ojTransform.transform.position, transform.up, Color.red);
-        if (Physics.Raycast(ojTransform.transform.position, -ojTransform.transform.up, out hit, 1))
+        if (Physics.Raycast(LeftFootTrnf.transform.position, -LeftFootTrnf.transform.up, out hit, 1))
         {
             if (hit.collider.gameObject.CompareTag("Ground"))
             {
-                t += Time.deltaTime;
-                if (t > footstepRate)
+                leftT += Time.deltaTime;
+                if (leftT > footstepRate)
                 {
                     Instantiate(footStepTexture, hit.point + Vector3.up * 0.01f, Quaternion.Euler(0, 0, 0));
-                    Debug.Log("Ground");
-                    t = 0;
+                    //Debug.Log("Ground");
+                    leftT = 0;
 
                 }
             }
+            
+        }
+        tAltSteps += Time.deltaTime;
+        if (tAltSteps > altSteps)
+        {
+            if (Physics.Raycast(RightFootTrnf.transform.position, -RightFootTrnf.transform.up, out hit, 1))
+            {
+                if (hit.collider.gameObject.CompareTag("Ground"))
+                {
+                    
+                        Instantiate(footStepTexture, hit.point + Vector3.up * 0.01f, Quaternion.Euler(0, 0, 0));
+                        
+                }
+                
+            }
+            tAltSteps = 0;
         }
         //Hands
         if (Physics.Raycast(ojTransform.transform.position, ojTransform.transform.forward, out hit, 1))
